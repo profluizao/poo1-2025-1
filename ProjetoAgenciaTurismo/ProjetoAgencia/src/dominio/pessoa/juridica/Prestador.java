1eb421acf3476b842df2eb2099797576870454b3
profluizao <prof.luiz.a.rodrigues.2018@gmail.com>
2025-04-20 12:25:20 -0400

    Outro teste agora com o VS Code

--- ProjetoAgenciaTurismo/ProjetoAgencia/src/dominio/pessoa/juridica/Prestador.java
+++ ProjetoAgenciaTurismo/ProjetoAgencia/src/dominio/pessoa/juridica/Prestador.java
@@ -4,12 +4,22 @@ import java.time.LocalDate;
 
 public class Prestador extends BasePessoaJuridica{
 
+    private double valorContratado;
+
+    public double getValorContratado() {
+        return valorContratado;
+    }
+
+    public void setValorContratado(double valorContratado) {
+        this.valorContratado = valorContratado;
+    }
+
     public Prestador(long codigo, LocalDate dataDeInclusao, LocalDate dataDeAlteracao, String nome, String telefone,
             String email, String cnpj, String inscricaoEstadual, LocalDate dataInicioVinculo,
             LocalDate dataFinalVinculo) {
         super(codigo, dataDeInclusao, dataDeAlteracao, nome, telefone, email, cnpj, inscricaoEstadual, dataInicioVinculo,
                 dataFinalVinculo);
-        //TODO Auto-generated constructor stub
-    }
+
+    }    
     
 }
