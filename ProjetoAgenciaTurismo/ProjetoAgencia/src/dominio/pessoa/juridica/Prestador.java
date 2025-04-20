8bc4c3dbb35b6a04fdc976edd29ebfaa8bab0d25
profluizao <prof.luiz.a.rodrigues.2018@gmail.com>
2025-04-19 21:44:57 -0400

    Outro teste do gittyup, agora com mais conteúdo.

--- ProjetoAgenciaTurismo/ProjetoAgencia/src/dominio/pessoa/juridica/Prestador.java
+++ ProjetoAgenciaTurismo/ProjetoAgencia/src/dominio/pessoa/juridica/Prestador.java
@@ -1,5 +1,15 @@
 package dominio.pessoa.juridica;
 
-public class Prestador {
+import java.time.LocalDate;
+
+public class Prestador extends BasePessoaJuridica{
+
+    public Prestador(long codigo, LocalDate dataDeInclusao, LocalDate dataDeAlteracao, String nome, String telefone,
+            String email, String cnpj, String inscricaoEstadual, LocalDate dataInicioVinculo,
+            LocalDate dataFinalVinculo) {
+        super(codigo, dataDeInclusao, dataDeAlteracao, nome, telefone, email, cnpj, inscricaoEstadual, dataInicioVinculo,
+                dataFinalVinculo);
+        //TODO Auto-generated constructor stub
+    }
     
 }
